c672ca7ab64e010943853da26becc7a969269e8e
Bartosz <bartoszkow03@gmail.com>
2024-09-20 13:57:00 +0200

    Add initial calculator display and some keypad buttons

--- Calculator-App/app/index.tsx
+++ Calculator-App/app/index.tsx
@@ -1,7 +1,76 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
 
 export default function Index() {
-  return <View></View>;
+  return (
+    <View style={styles.container}>
+      {/* Display */}
+      <View style={styles.displayContainer}>
+        <Text style={styles.historyText}>120 x 3 + 608 + 1080</Text>
+        <Text style={styles.resultText}>2,048</Text>
+      </View>
+
+      {/* Keypad */}
+      <View style={styles.keypadContainer}>
+        <View style={styles.row}>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttonText}>%</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttonText}>÷</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttonText}>×</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttonText}>⌫</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View>
+  );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#1c1c1c",
+    justifyContent: "space-between",
+    padding: 20,
+  },
+  displayContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "flex-end",
+    paddingRight: 10,
+  },
+  historyText: {
+    color: "#8888",
+    fontSize: 18,
+  },
+  resultText: {
+    color: "#fff",
+    fontSize: 48,
+    fontWeight: "bold",
+  },
+  keypadContainer: {
+    flex: 2,
+    justifyContent: "space-between",
+  },
+  button: {
+    width: 70,
+    height: 70,
+    backgroundColor: "#333",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 35,
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 24,
+  },
+  row: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    marginBottom: 15,
+  },
+});
