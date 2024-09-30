10bcc64361ae7c2380abd27e4fe3efbd7d7aca60
Bartosz <bartoszkow03@gmail.com>
2024-09-30 16:46:01 +0200

    Add HandleNumberInput function

--- Calculator-App/app/index.tsx
+++ Calculator-App/app/index.tsx
@@ -1,13 +1,32 @@
 import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
+import { useState } from "react";
 
 export default function Index() {
+  const [displayValue, setDisplayValue] = useState("0");
+  const [operator, setOperator] = useState(null);
+  const [firstValue, setFirstValue] = useState("");
+
+  const handleNumberInput = (num: number) => {
+    if (displayValue === "0") {
+      setDisplayValue(num.toString());
+    } else {
+      setDisplayValue(displayValue + num);
+    }
+  };
+
+  const ClearAll = () => {
+    setDisplayValue("0");
+    setOperator(null);
+    setFirstValue("");
+  };
+
   return (
     <View style={styles.container}>
       {/* Display */}
       <View style={styles.displayContainer}>
         <Text style={styles.historyText}>120 x 3 + 608 + 1080</Text>
-        <Text style={styles.resultText}>2,048</Text>
+        <Text style={styles.resultText}>{displayValue}</Text>
       </View>
 
       {/* Keypad */}
@@ -28,13 +47,22 @@ export default function Index() {
         </View>
 
         <View style={styles.row}>
-          <TouchableOpacity style={styles.button}>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => handleNumberInput(7)}
+          >
             <Text style={styles.buttonText}>7</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.button}>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => handleNumberInput(8)}
+          >
             <Text style={styles.buttonText}>8</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.button}>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => handleNumberInput(9)}
+          >
             <Text style={styles.buttonText}>9</Text>
           </TouchableOpacity>
           <TouchableOpacity style={styles.actionButton}>
@@ -43,13 +71,22 @@ export default function Index() {
         </View>
 
         <View style={styles.row}>
-          <TouchableOpacity style={styles.button}>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => handleNumberInput(4)}
+          >
             <Text style={styles.buttonText}>4</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.button}>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => handleNumberInput(5)}
+          >
             <Text style={styles.buttonText}>5</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.button}>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => handleNumberInput(6)}
+          >
             <Text style={styles.buttonText}>6</Text>
           </TouchableOpacity>
           <TouchableOpacity style={styles.actionButton}>
@@ -60,22 +97,34 @@ export default function Index() {
         <View style={styles.row}>
           <View style={styles.lastRowContainer}>
             <View style={styles.row}>
-              <TouchableOpacity style={styles.button}>
+              <TouchableOpacity
+                style={styles.button}
+                onPress={() => handleNumberInput(1)}
+              >
                 <Text style={styles.buttonText}>1</Text>
               </TouchableOpacity>
-              <TouchableOpacity style={styles.button}>
+              <TouchableOpacity
+                style={styles.button}
+                onPress={() => handleNumberInput(2)}
+              >
                 <Text style={styles.buttonText}>2</Text>
               </TouchableOpacity>
-              <TouchableOpacity style={styles.button}>
+              <TouchableOpacity
+                style={styles.button}
+                onPress={() => handleNumberInput(3)}
+              >
                 <Text style={styles.buttonText}>3</Text>
               </TouchableOpacity>
             </View>
 
             <View style={styles.row}>
-              <TouchableOpacity style={styles.actionButton}>
+              <TouchableOpacity style={styles.actionButton} onPress={ClearAll}>
                 <Text style={styles.buttonText}>AC</Text>
               </TouchableOpacity>
-              <TouchableOpacity style={styles.button}>
+              <TouchableOpacity
+                style={styles.button}
+                onPress={() => handleNumberInput(0)}
+              >
                 <Text style={styles.buttonText}>0</Text>
               </TouchableOpacity>
               <TouchableOpacity style={styles.button}>
